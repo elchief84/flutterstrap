c3328618d8db3d7ad7b813e1ca2a36cafd9c03f7
Vincenzo Romano <enzxx84@gmail.com>
2024-06-01 11:34:43 +0200

    example project done

--- example/lib/page_sample.dart
+++ example/lib/page_sample.dart
@@ -18,33 +18,77 @@ class _PageSampleState extends State<PageSample> {
             ResponsiveContainer(
               children: [
                 ResponsiveColumn(
-                  breakpoints: Breakpoints(colSm: 12, colXxl: 6),
+                  breakpoints: Breakpoints(colSm: 12, colLg:6, colXxl: 4),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Image.network('https://picsum.photos/2000?image=10'),
+                  ),
+                ),
+                ResponsiveColumn(
+                  breakpoints: Breakpoints(colSm: 12, colLg: 6, colXxl: 2),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Image.network('https://picsum.photos/2000?image=20'),
+                  ),
+                ),
+                ResponsiveColumn(
+                  breakpoints: Breakpoints(colSm: 12, colLg:6, colXxl: 4),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Image.network('https://picsum.photos/2000?image=30'),
+                  ),
+                ),
+                ResponsiveColumn(
+                  breakpoints: Breakpoints(colSm: 12, colLg: 6, colXxl: 2),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Image.network('https://picsum.photos/2000?image=40'),
+                  ),
+                ),
+              ],
+            ),
+
+            ResponsiveContainer(
+              children: [
+                ResponsiveColumn(
+                  breakpoints: Breakpoints(colLg: 12, colXxl: 6),
                   child: ResponsiveColumn(child: Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: Image.network('https://picsum.photos/2000?image=1'),
                   ),)
                 ),
-
                 ResponsiveColumn(
-                  breakpoints: Breakpoints(colSm: 12, colXxl: 6),
+                  breakpoints: Breakpoints(colLg: 12, colXxl: 6),
                   child: ResponsiveContainer(
                     children: [
-                      ResponsiveColumn(child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Image.network('https://picsum.photos/2000?image=10'),
-                      ),),
-                      ResponsiveColumn(child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Image.network('https://picsum.photos/2000?image=20'),
-                      ),),
-                      ResponsiveColumn(child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Image.network('https://picsum.photos/2000?image=30'),
-                      ),),
-                      ResponsiveColumn(child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Image.network('https://picsum.photos/2000?image=40'),
-                      ),)
+                      ResponsiveColumn(
+                        breakpoints: Breakpoints(colLg: 12, colXxl: 6),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Image.network('https://picsum.photos/2000?image=10'),
+                        ),
+                      ),
+                      ResponsiveColumn(
+                        breakpoints: Breakpoints(colLg: 12, colXxl: 6),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Image.network('https://picsum.photos/2000?image=20'),
+                        ),
+                      ),
+                      ResponsiveColumn(
+                        breakpoints: Breakpoints(colSm: 12, colXxl: 6),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Image.network('https://picsum.photos/2000?image=30'),
+                        ),
+                      ),
+                      ResponsiveColumn(
+                        breakpoints: Breakpoints(colSm: 12, colXxl: 6),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Image.network('https://picsum.photos/2000?image=40'),
+                        ),
+                      ),
                     ],
                   ),
                 ),
